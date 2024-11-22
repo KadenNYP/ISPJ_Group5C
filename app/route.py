04370792799dfb61a216d16b0ba2c45d95881b59
KadenNYP <230724y@mymail.nyp.edu.sg>
2024-11-22 09:01:45 +0800

    non-functional navbar

--- app/route.py
+++ app/route.py
@@ -2,6 +2,13 @@ from flask import Blueprint, render_template, redirect, url_for, flash
 
 route = Blueprint('route', __name__)
 
+"""@route.before_app_request
+def before_request():
+    if 'user' in session:
+        g.user = session['user']
+    else:
+        g.user = None"""
+
 @route.route('/')
 def index():
     return render_template("/home/index.html")

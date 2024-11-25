681df8e4b35d34a755c48ebb1db8bd011d17bcd4
KangYuXian <yuxiam14nbss2022@gmail.com>
2024-11-25 16:07:52 +0800

    Improve on the design for the login + signup pages & make small changes to the flash messages + home page (About Us section)

--- app/auth.py
+++ app/auth.py
@@ -6,14 +6,14 @@ auth = Blueprint('auth', __name__)
 @auth.route('signup', methods=['GET','POST'])
 def signup():
     # do something
-    flash('Account created. Thank you for signing up.\nWelcome to Healthcare Insurance!')
+    flash('Account created - Thank you for signing up')
     return render_template('user/signup.html')
 
 
 @auth.route('login', methods=['GET', 'POST'])
 def login():
     # do something
-    flash('Welcome to Healthcare Insurance!')
+    flash('Welcome to one of the best Insurance Company!')
     return render_template('user/login.html')
 
 

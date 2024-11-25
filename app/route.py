ec7fc6aad779080f01673ac381ee382ac028b324
KangYuXian <yuxiam14nbss2022@gmail.com>
2024-11-25 11:42:27 +0800

    Added a simple design to the Home Page & Improve on the design for Navbar & Created the files for the other pages for non-login users

--- app/route.py
+++ app/route.py
@@ -13,3 +13,18 @@ def before_request():
 @route.route('/')
 def index():
     return render_template("home/index.html")
+
+
+@route.route('/plans')
+def Plans_Overview():
+    return render_template("home/Plans.html")
+
+
+@route.route('/policy')
+def PolicyTerms():
+    return render_template("home/Policy&Terms.html")
+
+
+@route.route('/contact')
+def ContactSupport():
+    return render_template("home/Contact.html")

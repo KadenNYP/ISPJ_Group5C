c39e956734d76bb1f96b7e575ae4e8a1725dd5eb
KadenNYP <230724y@mymail.nyp.edu.sg>
2024-11-25 14:28:54 +0800

    make login page

--- app/auth.py
+++ app/auth.py
@@ -3,22 +3,22 @@ from flask import Blueprint, render_template, redirect, url_for, flash
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('signup', methods=['POST'])
+@auth.route('signup', methods=['GET','POST'])
 def signup():
     # do something
     flash('Account created. Thank you for signing up.\nWelcome to Healthcare Insurance!')
-    return redirect(url_for('index'))
+    return render_template('user/signup.html')
 
 
-@auth.route('login', methods=['POST'])
+@auth.route('login', methods=['GET', 'POST'])
 def login():
     # do something
     flash('Welcome to Healthcare Insurance!')
-    return redirect(url_for('index'))
+    return render_template('user/login.html')
 
 
 @auth.route('logout')
 def logout():
     # do something
     flash('You have been logged out.')
-    return redirect(url_for('index'))
+    return render_template('index.html')
